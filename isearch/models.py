c59dcb05c8c7012195734e09a6420c3069c0bad2
qwui <qwui301256@sina.com>
2019-01-20 00:18:16 +0800

    修改models.py 继承使用有问题

--- isearch/models.py
+++ isearch/models.py
@@ -19,64 +19,100 @@ class Exam(db.Model):
     name = db.Column(db.String(60), unique=True, nullable=False)
 
 
-class Score(db.Model):
-    """成绩表"""
-    id = db.Column(db.Integer, primary_key=True)
-    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
-    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
-    score = db.Column(db.Integer, default=0)
-    
-
 class MathScore(Score):
     """数学成绩表"""
     __tablename__ = "mathscore"
+        id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class ChineseScore(Score):
     """语文成绩表"""
     __tablename__ = "chinesescore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class EnglishScore(Score):
     """英语成绩表"""
     __tablename__ = "englishscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class WrittenScore(Score):
     """数学成绩表"""
     __tablename__ = "writtenscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class SpeakingScore(Score):
     """数学成绩表"""
     __tablename__ = "speakingscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class PoliticalScore(Score):
     """数学成绩表"""
     __tablename__ = "politicalscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class HistoryScore(Score):
     """数学成绩表"""
     __tablename__ = "historyscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class GeographyScore(Score):
     """数学成绩表"""
     __tablename__ = "geographyscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class BiologicalScore(Score):
     """生物成绩表"""
     __tablename__ = "biologicalscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class AntidrugScore(Score):
     """禁毒成绩表"""
     __tablename__ = "antidrugscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
 
 
 class TotalScore(Score):
     """总成绩表"""
     __tablename__ = "totalscore"
+    id = db.Column(db.Integer, primary_key=True)
+    stu_id = db.Column(db.String(20), db.ForeignKey("student.stu_id"), nullable=False)
+    exam_id = db.Column(db.Integer, db.ForeignKey("exam.id"), nullable=False)
+    score = db.Column(db.Integer, default=0)
